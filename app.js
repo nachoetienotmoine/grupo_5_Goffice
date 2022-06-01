498f6d4f97f7cc056a46019c62a1fb6f5c233057
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-05-31 21:02:19 -0300

    agrego ruta para carrito en el rutero

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/carrito.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, './views/carrito.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 app.listen(3000,() => {
     console.log('Corriendo en el puerto 3000');
 })
