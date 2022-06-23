8233adcc223a7aa86ad92d64346add9719ca5aab
unknown <nelson227@hotmail.es>
2022-06-23 20:53:49 -0300

    modularización de routers

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const routersRegister = require('./routers/register');
+const routersDetalle = require('./routers/detalle');
+const routersCarrito = require('./routers/carrito');
+const routersHome = require('./routers/home');
+const routersLogin = require('./routers/login');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, './views/registro.html')));
-app.get('/detalle', (req, res) => res.sendFile(path.join(__dirname, './views/detalle.html')));
-app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, './views/carrito.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
-app.get('/prueba', (req, res) => res.sendFile(path.join(__dirname, './views/prueba.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')));
+
+app.use('/registro', routersRegister);
+app.use('/detalle', routersDetalle);
+app.use('/carrito', routersCarrito);
+app.use('/', routersHome);
+app.use('/login', routersLogin);
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
 })
