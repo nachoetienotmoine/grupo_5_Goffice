2bc56aff642e65ee6a5c01fbe5c24b83ffc7af90
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-06-25 20:15:38 -0300

    avance ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(publicPath));
 app.use('/registro', routersRegister);
 app.use('/detalle', routersDetalle);
 app.use('/carrito', routersCarrito);
-/*app.use('/', routersHome);*/
+app.get('/', (req, res) => {res.render('index');})
 app.use('/login', routersLogin);
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
@@ -21,10 +21,7 @@ app.listen(process.env.PORT || 3000, function() {
 
 // instalandoEJS --->npm install ejs //
 app.set("view engine", "ejs"); // set entre otras cosas nos permite definir el motor de plantillas que queremos utilizar //
-//app.set('views', __dirname + ''); //configura la carpeta views-ejs para que reemplace a la que express lee por defecto.
+app.set('views', __dirname + '/views-ejs'); //configura la carpeta views-ejs para que reemplace a la que express lee por defecto.
 app.use(express.static('public')); //Con esto se le aclara a express donde están los recursos estáticos, para poder acceder a ellos desde las vistas
-app.get('/index.ejs', (req, res) => { // usamos esta parte de código para renderizar la vista de index.ejs (por ahora falta el rutero y controllers)
-    res.render('index');
-    }
-)
+
 
