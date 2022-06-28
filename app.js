ea593547d07641d7108efd9a2590734f17352a03
unknown <nelson227@hotmail.es>
2022-06-27 21:39:04 -0300

    appJS ruteo prodList

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const routersProdList = require('./routers/prodListApi');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+// instalandoEJS --->npm install ejs //
+
+app.set("view engine", "ejs");
+app.set('views', __dirname + '/views-ejs');
 
 app.use('/registro', routersRegister);
 app.use('/detalle', routersDetalle);
@@ -21,9 +25,7 @@ app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
 })
 
-// instalandoEJS --->npm install ejs //
-app.set("view engine", "ejs"); // set entre otras cosas nos permite definir el motor de plantillas que queremos utilizar //
-app.set('views', __dirname + '/views-ejs'); //configura la carpeta views-ejs para que reemplace a la que express lee por defecto.
-app.use(express.static('public')); //Con esto se le aclara a express donde están los recursos estáticos, para poder acceder a ellos desde las vistas
+
+
 
 
