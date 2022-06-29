41cad30a7f21431511b55c1ec86331dd64d7afab
unknown <nelson227@hotmail.es>
2022-06-28 22:28:30 -0300

    funcionalidad controllers

--- controllers/product/productos.js
+++ controllers/product/productos.js
@@ -1,12 +1,22 @@
+const productosData = require('../data/productosData');
+
 const productos = {
     listar: (req, res) => {
-        res.status(200).send("lista de productos");
+        res.status(200).send(productosData);
     },
     agregarProducto: (req, res) => {
         res.status(201).send("se ha creado el producto");
     },
     borrarProducto: (req, res) => {
-        const productoId = req.params.id;
+        const productoId = parseInt(req.params.id, 10);
+
+        for (let i = 0; i < productosData.length ; i++) {
+            if (productosData[i].id === productoId) {
+                productosData.splice(i,1)
+            }
+        }
+
+        console.log(productosData);
 
         res.send(`se ha borrado el producto con id ${productoId}`);
     },
