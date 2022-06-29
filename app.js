7628ee9b7e90b7a08ac739e064e76b1be3185c7f
unknown <nelson227@hotmail.es>
2022-06-28 21:30:51 -0300

    merge AppJS

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const routersCarrito = require('./routers/carritoApi');
 const routersHome = require('./routers/homeApi');
 const routersLogin = require('./routers/loginApi');
 const routersProdList = require('./routers/prodListApi');
-<<<<<<< HEAD
 const routersProductos = require('./routers/productos')
-=======
 const routersProdCRUD = require('./routers/prodCRUDApi');
->>>>>>> 5186647cb29ba63c678464125ac0f2165c308f6f
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
@@ -26,11 +23,8 @@ app.use('/carrito', routersCarrito);
 app.use('/', routersHome);
 app.use('/login', routersLogin);
 app.use('/prodList', routersProdList);
-<<<<<<< HEAD
 app.use('/productos', routersProductos)
-=======
 app.use('/prodCRUD', routersProdCRUD);
->>>>>>> 5186647cb29ba63c678464125ac0f2165c308f6f
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
 })
