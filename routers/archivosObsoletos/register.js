f812c4c57353e47956298e3e2b36235f5b21a917
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-07-15 22:09:06 -0300

    se agrega carpeta data y archivos JSON

--- routers/archivosObsoletos/register.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/registro.html')));
-
-module.exports = router;
