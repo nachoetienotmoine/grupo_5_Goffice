1a99557c58d984cc6c9d3deb823ccbaab5f6d1ba
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-07-17 17:12:50 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -4,7 +4,22 @@ const path = require('path');
 const productoFile = path.join(__dirname, '../data/product.json');
 const productosJ = JSON.parse(fs.readFileSync(productoFile, 'utf-8'));
 const productosController = {
-    
+    fileName: './data/product.json',
+    getData: function () {
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+
+    },
+    generateId: function () {
+        let allUsers = this.findAll();
+        let lastUser = allUsers.pop();
+        if (lastUser) {
+            return lastUser.id + 1;
+        }
+        return 1;
+    },
+    findAll: function () {
+        return this.getData();
+    },
     listar: (req, res) => {
         res.render("prodList", { productosJ: productosJ })
     },
@@ -17,40 +32,42 @@ const productosController = {
             oneUser.id === id);
         return userFound;
     },
-    crearProductosPost: (req , res) => {
+    crearProductosPost: (req, res) => {
         const name = req.body.name;
-		const price = req.body.price;
-		const discount = req.body.discount;
-		const category = req.body.category;
-		const description = req.body.description;
-		
+        const price = req.body.price;
+        const discount = req.body.discount;
+        const category = req.body.category;
+        const description = req.body.description;
 
-		// got them fused in a Object Literal;
-		const fuseData = { id: productosJ.length + 1,
-			name: name, price: price, discount: discount, category: category, description: description};
 
-		//
+        // got them fused in a Object Literal;
+        const fuseData = {
+            id: productosJ.length + 1,
+            name: name, price: price, discount: discount, category: category, description: description
+        };
 
-// 	Insert them, then they got sent away to the database.
-		productosJ.push(fuseData);
-		fs.writeFileSync(productoFile, JSON.stringify(productosJ), 'utf-8');
-		
-		//finally, you got kicked back to products for good.
-		  res.redirect("prodList")
-	},
-      
-    
-  
+        //
+
+        // 	Insert them, then they got sent away to the database.
+        productosJ.push(fuseData);
+        fs.writeFileSync(productoFile, JSON.stringify(productosJ), 'utf-8');
+
+        //finally, you got kicked back to products for good.
+        res.redirect("prodList")
+    },
+
+
+
 
     editProducto: (req, res) => {
         res.render("prodDetalle")
     },
 
     deleteProducto: function (id) {
-    let allProducts = this.findAll();
-    let finalProducts = allProducts.filter(oneProduct => oneProduct.id !== id);
-    fs.writeFileSync(this.fileName, JSON.stringify(finalProducts, null, ' '));
-    return true;
+        let allProducts = this.findAll();
+        let finalProducts = allProducts.filter (oneProduct => oneProduct.id !== id);
+        fs.writeFileSync(this.fileName, JSON.stringify(finalProducts, null, ' '));
+        return true;
     }
 
     // completar gonzalo//
@@ -59,23 +76,3 @@ const productosController = {
 
 
 module.exports = productosController;
-// listar: (req, res) => {
-    //     res.status(200).send(productosData);
-    // },
-
-    // agregarProducto: (req, res) => {
-    //     res.status(201).send("se ha creado el producto");
-    // },
-    // borrarProducto: (req, res) => {
-    //     const productoId = parseInt(req.params.id, 10);
-
-    //     for (let i = 0; i < productosData.length ; i++) {
-    //         if (productosData[i].id === productoId) {
-    //             productosData.splice(i,1)
-    //         }
-    //     }
-
-    //     console.log(productosData);
-
-    //     res.send(`se ha borrado el producto con id ${productoId}`);
-    // },
