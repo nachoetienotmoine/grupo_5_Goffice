f4b17627063cd819b91afb5e2e9dccbc82f5255b
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-07-17 03:43:17 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -4,21 +4,7 @@ const path = require('path');
 const productoFile = path.join(__dirname, '../data/product.json');
 const productosJ = JSON.parse(fs.readFileSync(productoFile, 'utf-8'));
 const productosController = {
-    fileName: './data/product.json',
-    getData: function () {
-        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'))
-    },
-    findAll: function () {
-        return this.getData();
-    },
-    generateId: function () {
-        let allUsers = this.findAll();
-        let lastUser = allUsers.pop();
-        if (lastUser) {
-            return lastUser.id + 1
-        }
-        return 1;
-    },
+    
     listar: (req, res) => {
         res.render("prodList", { productosJ: productosJ })
     },
@@ -33,27 +19,7 @@ const productosController = {
     },
     crearProductosPost: (req , res) => {
 
-        // I read inputs and then store them in variables.
-        const name = req.body.name;
-        const price = req.body.price;
-        const discount = req.body.discount;
-        const category = req.body.category;
-        const description = req.body.description;
-
-
-        // got them fused in a Object Literal;
-        const fuseData = {
-            id: productosJ.length + 1,
-            name: name, price: price, discount: discount, category: category, description: description
-        };
-
-        
-        // 	Insert them, then they got sent away to the database.
-        products.push(fuseData);
-        fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8');
-        console.log(req.file.image, req.body.image);
-        //finally, you got kicked back to products for good.
-        res.redirect('/prodList');
+        res.redirect("prodList")
     },
 
     editProducto: (req, res) => {
@@ -70,7 +36,7 @@ deleteProducto: function (id) {
     // completar gonzalo//
 
 }
-console.log(productosController.crearProductosPost({ name: "nelson", category: "pèrron", price: 3535466 }))
+
 
 module.exports = productosController;
 // listar: (req, res) => {
