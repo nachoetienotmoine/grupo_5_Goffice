e0f3be221ade9b0d870dcbbe72a2b743b0edc473
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-07-23 13:50:24 -0300

    actualizando punt 6 del sprint

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,7 @@ const path = require('path');
 const usersFile = path.join(__dirname, '../data/users.json');
 const usersJ = JSON.parse(fs.readFileSync(usersFile, 'utf-8'));
 const bcrypt = require('bcryptjs');
+
 const usersController = {
 
     users: (req, res) => {
