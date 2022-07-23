ae7013e980750c2ef820ea9e2450d18ecea515ef
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-07-23 19:23:39 -0300

    actualización avance sprint 5

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,29 +1,43 @@
+const fs = require("fs");
+const { parse } = require("path");
+const path = require('path');
+const usersFile = path.join(__dirname, '../data/users.json');
+const usersJ = JSON.parse(fs.readFileSync(usersFile, 'utf-8'));
 const bcryptjs = require("bcryptjs");
 
 const loginController = {
+    
+    users: (req, res) => {
+     
+        
+        const usersFile = path.join(__dirname, '../data/users.json');
+        const usersJ = JSON.parse(fs.readFileSync(usersFile, 'utf-8'));
+        res.render("login", { usersJ: usersJ })
+    },
 
     index: (req, res) => {
 
         res.render('login');
     },
-
-
     loginProces: (req, res) => {
+        console.log(usersJ)
+        console.log(req.body.email)
+        res.render(req.body.email)
 
-        res.send(req.body)
-        // console.log(req.body)
-        // let userToLogin = userJ.findByField("email", req.body.email);
-        // if (userToLogin) {
-        //     let itsOkeyThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
-        //     res.redirect("index");
-        // }
-        // res.sender("login", {
-        //     errors: {
-        //         email: {
-        //             msg: "Las Credenciales Son Invalidas"
-        //         }
-        //     }
-        // })
+        let userToLogin = usersJ.findByField("email", req.body.email);
+        if (userToLogin) {
+            let isOkeyThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+            if (isOkeyThePassword) {
+                return res.send ('ok puedes ingresar')
+            }
+        }
+        res.sender("login", {
+            errors: {
+                email: {
+                    msg: "Las Credenciales Son Invalidas"
+                }
+            }
+        })
     }
 
 }
