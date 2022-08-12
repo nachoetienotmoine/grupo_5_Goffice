cdf72f1b2dc8c080eb4e69652725df248201ae73
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-08-11 22:01:36 -0300

    cambios

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
+const fs = require("fs");
+const { parse } = require("path");
+const path = require('path');
+const productoFile = path.join(__dirname, '../data/product.json');
+const productosJ = JSON.parse(fs.readFileSync(productoFile, 'utf-8'));
+
+
+
 const homeController = {
 
     index: (req, res) => {
-
-        res.render('index');
+        const productoFile = path.join(__dirname, '../data/product.json');
+        const productosJ = JSON.parse(fs.readFileSync(productoFile, 'utf-8'));
+        res.render("index", { productosJ: productosJ })
     }
 
 }
