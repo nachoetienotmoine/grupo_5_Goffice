0946c15d84751854714bcb2a6b709b928629e92c
unknown <nelson227@hotmail.es>
2022-08-19 18:28:11 -0300

    Database sql

--- database/goffice.sql
+++ database/goffice.sql
@@ -1,18 +1,23 @@
+DROP DATABASE IF EXISTS goffice;
 CREATE SCHEMA `goffice` ;
-
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `gender` varchar(45) NOT NULL,
-  `image` varchar(45) NOT NULL,
-  `id_category_user` varchar(45) NOT NULL,
-  UNIQUE KEY `idusers_UNIQUE` (`id`),
-  UNIQUE KEY `id_category_user_UNIQUE` (`id_category_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
+USE goffice;
+CREATE TABLE `user_category` (
+  `id_users_category` int NOT NULL AUTO_INCREMENT,
+  `id_users` int NOT NULL,
+  `category_users` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_users_category`),
+  UNIQUE KEY `idUsers Category_UNIQUE` (`id_users_category`),
+  UNIQUE KEY `Id Users_UNIQUE` (`id_users`),
+  KEY `Id Users_idx` (`id_users`)
+);
+CREATE TABLE `category_products` (
+  `id_category_products` int NOT NULL AUTO_INCREMENT,
+  `id_products` int DEFAULT NULL,
+  `Category` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_category_products`),
+  UNIQUE KEY `idCategory Productos_UNIQUE` (`id_category_products`),
+  KEY `Id products_idx` (`id_products`)
+);
 CREATE TABLE `Products` (
   `id` int NOT NULL AUTO_INCREMENT,
   `products_name` varchar(45) NOT NULL,
@@ -27,23 +32,16 @@ CREATE TABLE `Products` (
   UNIQUE KEY `Products Name_UNIQUE` (`products_name`),
   UNIQUE KEY `Description_UNIQUE` (`description`),
   CONSTRAINT `id_category_product` FOREIGN KEY (`id`) REFERENCES `category_products` (`id_category_products`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-CREATE TABLE `user_category` (
-  `id_users_category` int NOT NULL AUTO_INCREMENT,
-  `id_users` int NOT NULL,
-  `category_users` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_users_category`),
-  UNIQUE KEY `idUsers Category_UNIQUE` (`id_users_category`),
-  UNIQUE KEY `Id Users_UNIQUE` (`id_users`),
-  KEY `Id Users_idx` (`id_users`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-CREATE TABLE `category_products` (
-  `id_category_products` int NOT NULL AUTO_INCREMENT,
-  `id_products` int DEFAULT NULL,
-  `Category` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_category_products`),
-  UNIQUE KEY `idCategory Productos_UNIQUE` (`id_category_products`),
-  KEY `Id products_idx` (`id_products`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+);
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `image` varchar(45) NOT NULL,
+  `id_category_user` varchar(45) NOT NULL,
+  UNIQUE KEY `idusers_UNIQUE` (`id`),
+  UNIQUE KEY `id_category_user_UNIQUE` (`id_category_user`)
+);
