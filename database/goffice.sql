9110aa754bc6bf4885a236a991a1cd5326e33c4c
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-08-19 22:18:15 +0200

    Update goffice.sql
    update base datos
    nahco cogn

--- database/goffice.sql
+++ database/goffice.sql
@@ -8,8 +8,9 @@ CREATE TABLE `users` (
   `password` varchar(45) NOT NULL,
   `gender` varchar(45) NOT NULL,
   `image` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idusers_UNIQUE` (`id`)
+  `id_category_user` varchar(45) NOT NULL,
+  UNIQUE KEY `idusers_UNIQUE` (`id`),
+  UNIQUE KEY `id_category_user_UNIQUE` (`id_category_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `Products` (
@@ -20,10 +21,22 @@ CREATE TABLE `Products` (
   `discount` int NOT NULL,
   `image` varchar(500) NOT NULL,
   `stock` int NOT NULL,
+  `id_category_product` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idProducts_UNIQUE` (`id`),
   UNIQUE KEY `Products Name_UNIQUE` (`products_name`),
-  UNIQUE KEY `Description_UNIQUE` (`description`)
+  UNIQUE KEY `Description_UNIQUE` (`description`),
+  CONSTRAINT `id_category_product` FOREIGN KEY (`id`) REFERENCES `category_products` (`id_category_products`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `user_category` (
+  `id_users_category` int NOT NULL AUTO_INCREMENT,
+  `id_users` int NOT NULL,
+  `category_users` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_users_category`),
+  UNIQUE KEY `idUsers Category_UNIQUE` (`id_users_category`),
+  UNIQUE KEY `Id Users_UNIQUE` (`id_users`),
+  KEY `Id Users_idx` (`id_users`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `category_products` (
@@ -33,14 +46,4 @@ CREATE TABLE `category_products` (
   PRIMARY KEY (`id_category_products`),
   UNIQUE KEY `idCategory Productos_UNIQUE` (`id_category_products`),
   KEY `Id products_idx` (`id_products`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
-CREATE TABLE `user_category` (
-  `id_users_category` int NOT NULL AUTO_INCREMENT,
-  `id_users` int NOT NULL,
-  `category_users` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_users_category`),
-  UNIQUE KEY `idUsers Category_UNIQUE` (`id_users_category`),
-  UNIQUE KEY `Id Users_UNIQUE` (`Id_users`),
-  KEY `Id Users_idx` (`Id_users`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
