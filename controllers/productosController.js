d331e7ec70a301a352cbaf5616746b023eac39f0
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-08-24 00:53:55 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -56,34 +56,26 @@ const productosController = {
         let productImage = path.format({root: '/ignored', dir: path.join(__dirname, '..', '/public/images') , base: 'IMG_2546.jpg'});
         res.render('admin/prodEdit', { productoEncontrado: products, productImage: productImage, categoryProducts: categoryProducts });
     },
+    update: async (req, res) => {
+        let {name, category, description, discount, price, stock} = req.body;
+        Products.update(
+            {
+                name: name,
+                description: description,
+                discount: discount,
+                price: price,
+                image: req.file.originalname,
+                stock: stock,
+                id_products_category: category
+            },
+            {
+                where: {id: req.params.id}
+            }
+        );
+        res.redirect('/productos/' + req.params.id);
+    },
 
-    update: (req, res) => {
-		const id = parseInt(req.params.id);
-		const name = req.body.name;
-		const price = parseInt(req.body.price);
-		const discount = req.body.discount;
-		const category = req.body.category;
-		const description = req.body.description;
-        const stock = req.body.stock;
-        const image = req.file.originalname;
-		const productToUpdate = productosJ.filter(product => product.id === id);
-
-		productToUpdate[0].name = name;
-		productToUpdate[0].price = price;
-		productToUpdate[0].discount = discount;
-		productToUpdate[0].category = category;
-		productToUpdate[0].description = description;
-        productToUpdate[0].stock = stock;
-        productToUpdate[0].image = image;
-
-		productosJ[id-1] = productosJ[id-1] = productToUpdate[0];
-
-		fs.writeFileSync(productoFile, JSON.stringify(productosJ), 'utf-8');
-		
-
-
-		res.redirect('/productos/' + id);
-	},
+   
 
     deleteProducto: (req, res) => {
         const productId = parseInt(req.params.id, 10);
