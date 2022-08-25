63afc51c84c7674329f3ddee76be24e9cf31244b
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-08-25 00:19:05 -0300

    cambios

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -1,31 +1,20 @@
-const fs = require("fs");
-const path = require('path');
-let userJson = path.join(__dirname, '../data/users.json');
-let userOl = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
-
-function userLoggedMiddleware(req, res, next) {
+const db = require('../database/models');
+const Users = db.User;
+async function userLoggedMiddleware(req, res, next) {
     res.locals.isLogged = false;
-
     let emailInCookie = req.cookies.userEmail;
     let userFromCookie;
-
-    for (let i = 0; i < userOl.length; i++){
-        if (userOl[i].email === emailInCookie) {
-            userFromCookie = userOl[i];
-        }
+    if (emailInCookie != null){
+        userFromCookie = await Users.findOne({where: {email: emailInCookie}});
     }
-
     if (userFromCookie) {
         req.session.userLogged = userFromCookie;
     }
-
     if (req.session.userLogged) {
         res.locals.isLogged = true;
         res.locals.userLogged = req.session.userLogged;
     }
-
 // cambiar , por error //
     next();
 }
-
 module.exports = userLoggedMiddleware;
