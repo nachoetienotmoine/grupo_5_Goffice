b112c2c3aefff329624916eb5cb98431ba0a6137
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-08-26 14:14:55 -0300

    cambios

--- routers/carritoApi.js
+++ routers/carritoApi.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const carritoController = require('../controllers/carritoController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/', carritoController.index);
+router.get('/',authMiddleware ,carritoController.index);
 
 module.exports = router;
