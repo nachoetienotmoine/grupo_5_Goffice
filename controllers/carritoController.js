53a538facb3c2a3a47d7d2c9b061cb9a8999b752
unknown <nelson227@hotmail.es>
2022-09-02 07:43:16 -0300

    cart crud demo 1

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -11,24 +11,53 @@ const carritoController = {
     },
 
     agregar: async (req, res) => {
-        const id = req.params.id;
-        const userId = req.session.userLogged.dataValues.id;
+        const id = parseInt(req.params.id);
+        const userId = parseInt(req.session.userLogged.dataValues.id);
 
         const existeCarrito = await cart.findOne({where: {users_id: userId}});
 
+        // const cartExists = await cart.findOne({where: {users_id: 4}});
+        // console.log(await cartExists.addProducts([5,5,5]));
+
         if (!existeCarrito){
-            cart.create({
-                total_products: 0,
-                total_price: 0,
-                payment_methods_id: 2,
-                users_id: userId,
-                Products: [
-                    {carts_id:userId,
-                    products_id: id}
-                ]
-            }, {
-                include: 'Products'
+            await cart.create({
+            total_products: 0,
+            total_price: 0,
+            payment_methods_id: 2,
+            users_id: userId
             });
+
+            const userCart = await cart.findOne({where: {users_id: userId}});
+
+            await userCart.addProducts([id]);
+
+            const cartsProducts = await userCart.getProducts();
+            let productsWholeValue = 0;
+            let productsTotal = 0;
+            await cartsProducts.forEach((product) => 
+            {productsWholeValue = productsWholeValue + product.price, productsTotal++});
+
+            await cart.update({
+                total_products: productsTotal,
+                total_price: productsWholeValue
+                },{
+                    where: { users_id: userId }
+                });
+
+        }else {
+            await existeCarrito.addProducts([id]);
+            const cartsProducts = await existeCarrito.getProducts();
+            let productsWholeValue = 0;
+            let productsTotal = 0;
+            await cartsProducts.forEach((product) => 
+            {productsWholeValue = productsWholeValue + product.price, productsTotal++});
+
+            await cart.update({
+                total_products: productsTotal,
+                total_price: productsWholeValue
+                },{
+                    where: { users_id: userId }
+                });
         }
     },
 
