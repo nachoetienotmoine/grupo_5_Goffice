04ee91666a635464d3136c1152771b58e34d8ebb
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-09-14 21:26:06 -0300

    cambios , arereglaod checkout

--- public/js/CheckOut.js
+++ public/js/CheckOut.js
@@ -2,83 +2,127 @@ const form = document.querySelectorAll('.formcheckout');
 const formCredit = document.querySelector('.credit');
 const formCash = document.querySelector('.cash');
 const formDebit = document.querySelector('.debit');
-
+const formTrans = document.querySelector('.trans')
+const selectMetodoPago = document.querySelector('#metodopago');
 formCash.classList.add('hide');
 formDebit.classList.add('hide');
+formCredit.classList.add('hide');
+formTrans.classList.add('hide');
 
-let formOnDisplay = formCredit;
 
-formCredit.addEventListener('click', (e) => {
-
-    let selectedValue;
-
-    for (let i = 0; i < form.length ;i++){
-        e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
-    }
+selectMetodoPago.addEventListener('change', (e)=>{
     
-    if (selectedValue.value == "efectivo"){
-        formCredit.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCash.classList.remove('hide');
-    }else if (selectedValue.value == "crédito"){
-        formCash.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCredit.classList.remove('hide');
-    }else {
-        formCash.classList.add('hide');
-        formCredit.classList.add('hide');
-        formDebit.classList.remove('hide');
-    }
+    switch (e.target.value) {
+        
+            case "efectivo":
+                formCredit.classList.add('hide');
+                formCash.classList.remove('hide');
+                formDebit.classList.add('hide');
+                formTrans.classList.add('hide');
+             
+                break;
+        
+                case "credito":
+                    formCash.classList.add('hide');
+                    formCredit.classList.remove('hide');
+                    formDebit.classList.add('hide');
+                    formTrans.classList.add('hide');
+                   
+                    break;
 
-});
+                    case "debito":
+                    formCash.classList.add('hide');
+                    formCredit.classList.add('hide');
+                    formDebit.classList.remove('hide');
+                    formTrans.classList.add('hide');
+                    break;
 
-formCash.addEventListener('click', (e) => {
+                    case "transferencia":
+                    formCash.classList.add('hide');
+                    formCredit.classList.add('hide');
+                    formDebit.classList.add('hide');
+                    formTrans.classList.remove('hide');
+                   
+                    break;
+            
+                
+        }
+    ;
+})
 
-    let selectedValue;
 
-    for (let i = 0; i < form.length ;i++){
-        e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
-    }
+
+// formCredit.addEventListener('click', (e) => {
+
+//     let selectedValue;
+
+//     for (let i = 0; i < form.length ;i++){
+//         e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
+//     }
     
-    if (selectedValue.value == "efectivo"){
-        formCredit.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCash.classList.remove('hide');
-    }else if (selectedValue.value == "crédito"){
-        formCash.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCredit.classList.remove('hide');
-    }else {
-        formCash.classList.add('hide');
-        formCredit.classList.add('hide');
-        formDebit.classList.remove('hide');
-    }
+//     if (selectedValue.value == "efectivo"){
+//         formCredit.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCash.classList.remove('hide');
+//     }else if (selectedValue.value == "crédito"){
+//         formCash.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCredit.classList.remove('hide');
+//     }else {
+//         formCash.classList.add('hide');
+//         formCredit.classList.add('hide');
+//         formDebit.classList.remove('hide');
+//     }
+// console.log(selectedValue)
+// });
 
-});
+// formCash.addEventListener('click', (e) => {
 
-formDebit.addEventListener('click', (e) => {
+//     let selectedValue;
 
-    let selectedValue;
-
-    for (let i = 0; i < form.length ;i++){
-        e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
-    }
+//     for (let i = 0; i < form.length ;i++){
+//         e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
+//     }
     
-    if (selectedValue.value == "efectivo"){
-        formCredit.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCash.classList.remove('hide');
-    }else if (selectedValue.value == "crédito"){
-        formCash.classList.add('hide');
-        formDebit.classList.add('hide');
-        formCredit.classList.remove('hide');
-    }else {
-        formCash.classList.add('hide');
-        formCredit.classList.add('hide');
-        formDebit.classList.remove('hide');
-    }
+//     if (selectedValue.value == "efectivo"){
+//         formCredit.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCash.classList.remove('hide');
+//     }else if (selectedValue.value == "crédito"){
+//         formCash.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCredit.classList.remove('hide');
+//     }else {
+//         formCash.classList.add('hide');
+//         formCredit.classList.add('hide');
+//         formDebit.classList.remove('hide');
+//     }
+//     console.log(selectedValue)
+// });
 
-});
+// formDebit.addEventListener('click', (e) => {
+
+//     let selectedValue;
+
+//     for (let i = 0; i < form.length ;i++){
+//         e.path[0][i].selected == true ? selectedValue = e.path[0][i] : "";
+//     }
+    
+//     if (selectedValue.value == "efectivo"){
+//         formCredit.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCash.classList.remove('hide');
+//     }else if (selectedValue.value == "crédito"){
+//         formCash.classList.add('hide');
+//         formDebit.classList.add('hide');
+//         formCredit.classList.remove('hide');
+//     }else {
+//         formCash.classList.add('hide');
+//         formCredit.classList.add('hide');
+//         formDebit.classList.remove('hide');
+//     }
+
+// });
 
 
 
