5c0b63ec54d963a48b9a4998b232968bc66ef518
unknown <nelson227@hotmail.es>
2022-09-18 06:53:04 -0300

    less button working

--- public/js/Carrito.js
+++ public/js/Carrito.js
@@ -1,16 +1,51 @@
-const quantityLess = document.querySelector('.cantidadMenos');
+const quantityLess = document.querySelectorAll('.cantidadMenos');
 const quantityMore = document.querySelectorAll('.cantidadMas');
 const quantityNumber = document.querySelectorAll('.cantidad');
 const productAddedName = document.querySelectorAll('.nombreProductoAgregado');
 const productAddedPrice = document.querySelectorAll('.precioProductoAgregado');
 const preciocarrito = document.querySelectorAll('.PrecioenCarrito')
 
-// quantityLess.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     let numberValue = quantityNumber.value;
-//     quantityNumber.value = --numberValue;
-//     console.log('less!');
-// });
+for(let i = 0; i < quantityLess.length; i++){
+    quantityLess[i].addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let numberValue = quantityNumber[i].value;
+
+        let productElement = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+        let productName = productElement.firstElementChild.textContent.trim();
+
+        async function oneProduct(){
+            let product = await fetch('/baseDeDatosInfo/findOne', 
+            {method:'POST',headers: {'Content-Type':'application/json'},body: JSON.stringify({product: productName})})
+            .then(res => res.json()).then(data => {return data;});
+
+            return product;
+        }
+
+        oneProduct().then(res => {
+            if (numberValue > 1){
+                quantityNumber[i].value = --numberValue;
+                for (let i = 0; i < productAddedName.length; i++){
+                    if (productName == productAddedName[i].textContent.trim()){
+                        let price = [];
+                        let gettingRidOfDolarSign = [];
+                        let originalPrice = res.price;
+
+                        price.push(preciocarrito[i].textContent);
+                        gettingRidOfDolarSign.push(price[0].trimStart().slice(1,price[0].length));
+        
+                        gettingRidOfDolarSign = parseInt(gettingRidOfDolarSign);
+                        price = (gettingRidOfDolarSign * numberValue) - originalPrice + originalPrice;
+                        productAddedPrice[i].innerHTML = "$" + price;
+                    }
+                }
+            }
+        }
+        )
+    
+        console.log('less!');
+    });
+}
 
 for(let i = 0; i < quantityMore.length; i++){
     quantityMore[i].addEventListener('click', (e) => {
