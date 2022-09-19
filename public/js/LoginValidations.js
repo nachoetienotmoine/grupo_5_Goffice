e1ecc7a1be2db88078f9064b6e47602696bb3f40
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-09-19 00:26:00 -0300

    validaciones front login

--- public/js/LoginValidations.js
+++ public/js/LoginValidations.js
@@ -1,1 +1,36 @@
 
+let formulario = document.querySelector("form.login_validation");
+
+formulario.addEventListener("submit", function(e){
+
+    let errors = [];
+
+    let campoEmail = document.querySelector(".login-val");
+
+
+    if (campoEmail.value == "") {
+        errors.push("El campo Email tiene que estar completo")        
+    } else if (campoEmail.value.length < 3) {
+        errors.push("El campo Email tiene que ser mayor válido")
+    }       
+
+    let campoPassword = document.querySelector(".login-pass");
+
+
+    if (campoPassword.value == "") {
+        errors.push("El campo Password tiene que estar completo")        
+    } else if (campoPassword.value.length < 3) {
+        errors.push("El campo Password tiene que ser mayor a 8")
+    }  
+    
+    if(errors.length > 0){
+        e.preventDefault();
+    }
+    let ulErrores = document.querySelector("div.errors-front ul")
+    for (let i = 0; i < errors.length; i++) {
+        
+        ulErrores.innerHTML += "<li>" + errors[i] + "</li>"
+        
+    }
+})
+
