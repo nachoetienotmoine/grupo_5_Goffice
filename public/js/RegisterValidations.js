57cd9610482bdd779d1fcfb5ff6456f6755112a8
unknown <nelson227@hotmail.es>
2022-09-19 23:48:05 -0300

    register no empty string

--- public/js/RegisterValidations.js
+++ public/js/RegisterValidations.js
@@ -11,25 +11,31 @@ function isEmpty(input){
         errors[i] === input ? alreadyChecked = true : "";
     }
 
-        if (input.value === "" && !alreadyChecked){
-            errors.push('Debes completar el nombre');
-            return true;
-        }else if (input.value != ""){
-            for (let i = 0; i < errors.length; i++){
-                if (errors[i] === input){
-                    errors.splice(0);
-                    return false;
-                }
+    if (input.value === "" && !alreadyChecked){
+        errors.push('Debes completar el nombre');
+        return true;
+    }else if (input.value != ""){
+        for (let i = 0; i < errors.length; i++){
+            if (errors[i] === input){
+                errors.splice(0);
+                return false;
             }
         }
+    }    
 }
 
 first_Name.addEventListener('blur', (e) => {
     if (isEmpty(e.target) === true){
         error_field[0].innerHTML = errors[0];
         error_field[0].style.display = "block";
-    }else{
+    }else if (e.target.value.length <= 2){
+        errors.push('El nombre debe contener al menos 2 caracteres');
+        error_field[0].innerHTML = errors[0];
+        error_field[0].style.display = "block";
+    }
+    else{
         error_field[0].style.display = "none";
+        console.log('pene');
     }
 
 });
