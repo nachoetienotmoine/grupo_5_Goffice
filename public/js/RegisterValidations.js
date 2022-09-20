0f57a30d59930dd60df4e3b61238a2d70a92d4d2
unknown <nelson227@hotmail.es>
2022-09-20 07:21:36 -0300

    I DID IT! ALMOUST DONE THE FRAKING REGISTER (THERE'S THINGS TO DO ANYWAY)

--- public/js/RegisterValidations.js
+++ public/js/RegisterValidations.js
@@ -2,24 +2,44 @@ const error_field = document.querySelectorAll('.error_field')
 const first_Name = document.querySelector('#name');
 const last_name = document.querySelector('#lastname');
 
-let errors = [];
+function Errors(input, message, errorField){
+    this.input = input,
+    this.message = message,
+    this.errorField = errorField;
+}
 
-function isEmpty(input, name){
+let errorsList = [];
+
+function isEmpty(input, field){
+
+    let fieldName = field.name;
 
     let alreadyChecked = false;
-    for (let i = 0; i < errors.length; i++){
-        errors[i] === `Debes completar el ${name.name}` ? alreadyChecked = true : "";
+
+    for (let i = 0; i < errorsList.length; i++){
+        if (errorsList[i][fieldName]){
+            errorsList[i][fieldName].message === `Debes completar el ${fieldName}` ? alreadyChecked = true : "";
+        }
 
     }
 
     if (input === "" && !alreadyChecked){
-        errors.push(`Debes completar el ${name.name}`);
+        let input_Name = {
+            [fieldName]: new Errors (field, `Debes completar el ${field.name}`, error_field[0])
+        };
+
+        errorsList.push(input_Name);
         return true;
     }else if (input != ""){
-        for (let i = 0; i < errors.length; i++){
-            if (errors[i] === `Debes completar el ${name.name}`){
-                errors.splice(0,errors.length);
-                return false;
+        for (let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].message === `Debes completar el ${fieldName}`){
+                    Object.keys(errorsList).map(function(i) {
+                        errorsList.splice(i,1);
+                      });
+                    console.log(errorsList);
+                    return false;
+                }
             }
         }
     }else{
@@ -27,73 +47,80 @@ function isEmpty(input, name){
     }   
 }
 
-function islongEnough(input,name){
-    let lengthAlreadyChecked = false;
-    for (let i = 0; i < errors.length; i++){   
-        errors[i] === `El ${name.name} debe contener al menos 2 caracteres` ? lengthAlreadyChecked = true : "";
-    }
+// function islongEnough(input,name){
+//     let lengthAlreadyChecked = false;
+//     for (let i = 0; i < errors.length; i++){   
+//         errors[i] === `El ${name.name} debe contener al menos 2 caracteres` ? lengthAlreadyChecked = true : "";
+//     }
 
-    if (input.length < 2 && !lengthAlreadyChecked){
-        errors.push(`El ${name.name} debe contener al menos 2 caracteres`);
-        return true;
-    }else if (input.length > 2){
-        for (let i = 0; i < errors.length; i++){
-            if (errors[i] === `El ${name.name} debe contener al menos 2 caracteres`){
-                errors.splice(0,errors.length);
-                return false;
-            }
-        }
-    }else {
-        return true;
-    }
-}
+//     if (input.length < 2 && !lengthAlreadyChecked){
+//         errors.push(`El ${name.name} debe contener al menos 2 caracteres`);
+//         return true;
+//     }else if (input.length > 2){
+//         for (let i = 0; i < errors.length; i++){
+//             if (errors[i] === `El ${name.name} debe contener al menos 2 caracteres`){
+//                 errors.splice(1,1);
+//                 return false;
+//             }
+//         }
+//     }else {
+//         return true;
+//     }
+// }
 
 first_Name.addEventListener('blur', (e) => {
     let inputValue = e.target.value.trim();
+    let field = first_Name;
 
-    if (isEmpty(inputValue, first_Name) === true){
-        error_field[0].innerHTML = errors[0];
+    if (isEmpty(inputValue, field)){
+        let fieldName = field.name;
+        let errorMessage;
+
+        for ( let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].input.name == fieldName){
+                    errorMessage = errorsList[i][fieldName].message;
+                }
+            }
+        }
+
+        error_field[0].innerHTML = errorMessage;
         error_field[0].style.display = "block";
+        console.log(errorsList);
     }else{
         error_field[0].style.display = "none";
         first_Name.value = inputValue;
     }
 
-    if (islongEnough(inputValue,first_Name)){
-        error_field[0].innerHTML = errors[0];
-        error_field[0].style.display = "block";
-    }else {
-        errors.splice(0);
-        error_field[0].style.display = "none";
-    }
-
-    console.log(errors.length, e.target.value);
-    console.log(errors);
+    // if (islongEnough(inputValue)){
+    //     error_field[0].innerHTML = errors[0];
+    //     error_field[0].style.display = "block";
+    // }else {
+    //     error_field[0].style.display = "none";
+    // }
 });
 
 last_name.addEventListener('blur', (e) => {
     let inputValue = e.target.value.trim();
+    let field = last_name;
 
-    if (isEmpty(inputValue, last_name) === true){
-        error_field[1].innerHTML = errors[0];
+    if (isEmpty(inputValue, field)){
+        let fieldName = field.name;
+        let errorMessage;
+
+        for ( let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].input.name == fieldName){
+                    errorMessage = errorsList[i][fieldName].message;
+                }
+            }
+        }
+
+        error_field[1].innerHTML = errorMessage;
         error_field[1].style.display = "block";
+        console.log(errorsList);
     }else{
         error_field[1].style.display = "none";
         last_name.value = inputValue;
     }
-
-    if (islongEnough(inputValue,last_name)){
-        error_field[1].innerHTML = errors[0];
-        error_field[1].style.display = "block";
-    }else {
-        errors.splice(0);
-        error_field[1].style.display = "none";
-    }
-
-    console.log(errors.length, e.target.value);
-    console.log(errors);
 });
-
-
-
-
