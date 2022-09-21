bda886588cefcd5214c56363e41e12033cb7d454
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-09-21 19:43:31 +0200

    carrusel carrito vacio
    vista previa

---
+++ public/js/slider.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  allowTouchMove: true,
+  slidesPerView: 4,
+  spaceBetween:30,
+  
+  
+
+  // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /* And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+});
