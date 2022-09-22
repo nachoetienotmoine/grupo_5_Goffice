f477affa906712650f7d1312c04d4c6ba936478b
unknown <nelson227@hotmail.es>
2022-09-22 00:21:01 -0300

    email almoust validated

--- public/js/RegisterValidations.js
+++ public/js/RegisterValidations.js
@@ -2,6 +2,7 @@ const formR = document.querySelector('.register_form');
 const error_field = document.querySelectorAll('.error_field')
 const first_Name = document.querySelector('#name');
 const last_name = document.querySelector('#lastname');
+const email = document.querySelector('#email');
 
 function Errors(input, message, errorField){
     this.input = input,
@@ -84,19 +85,56 @@ function islongEnough(input, field){
     }   
 }
 
-function validEmail(email) {
+function notvalidEmail(input, field) {
     var filter = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
-    return String(email).search (filter) != -1;
+    // return String(input).search (filter) != -1;
+
+    ////*//////*/////
+
+    let errorField = field.parentElement.nextElementSibling;
+    let fieldName = field.name;
+
+    let alreadyChecked = false;
+
+    for (let i = 0; i < errorsList.length; i++){
+        if (errorsList[i][fieldName]){
+            errorsList[i][fieldName].message === `El ${fieldName} debe contener un formato valido` ? alreadyChecked = true : "";
+        }
+
+    }
+
+    if (String(input).search (filter) != -1 && !alreadyChecked){
+        let input_Name = {
+            [fieldName]: new Errors (field, `El ${fieldName} debe contener un formato valido`, errorField)
+        };
+
+        errorsList.push(input_Name);
+        return true;
+    }else if (String(input).search (filter) != -1){
+        for (let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].message === `El ${field.name} debe contener un formato valido`){
+                    Object.keys(errorsList).map(function(i) {
+                        errorsList.splice(i,1);
+                      });
+                    return false;
+                }
+            }
+        }
+    }else{
+        return true;
+    }
 }
 
 formR.addEventListener('submit', (e) => {
     e.preventDefault();
     let firstNameSelected = first_Name.classList.contains('Selected');
     let lastNameSelected = last_name.classList.contains('Selected');
+    let emailSelected = email.classList.contains('Selected');;
 
     if (errorsList.length > 0){
         console.log("there's errors inside the form");
-    }else if (firstNameSelected && lastNameSelected){
+    }else if (firstNameSelected && lastNameSelected && emailSelected){
         console.log("no errors inside :D");
     }
 });
@@ -191,4 +229,53 @@ last_name.addEventListener('blur', (e) => {
         error_field[1].style.display = "none";
         last_name.value = inputValue;
     }
+});
+
+email.addEventListener('blur', (e) => {
+    let inputValue = e.target.value.trim();
+    let field = email;
+    email.classList.add('Selected');
+
+    if (isEmpty(inputValue, field)){
+        let fieldName = field.name;
+        let errorMessage;
+
+        for ( let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].input.name == fieldName){
+                    errorMessage = errorsList[i][fieldName].message;
+                }
+            }
+        }
+
+        error_field[2].innerHTML = errorMessage;
+        error_field[2].style.display = "block";
+        console.log(errorsList);
+    }else{
+        error_field[2].style.display = "none";
+        email.value = inputValue;
+    }
+    console.log(notvalidEmail(inputValue, field));
+    if (notvalidEmail(inputValue, field)){
+        let errorMessage;
+        let fieldName = field.name;
+
+        for ( let i = 0; i < errorsList.length; i++){
+            if (errorsList[i][fieldName]){
+                if (errorsList[i][fieldName].input.name == fieldName){
+                    errorMessage = errorsList[i][fieldName].message;
+                }
+            }
+        }
+
+        error_field[2].innerHTML = errorMessage;
+        error_field[2].style.display = "block";
+        console.log(errorsList);
+    }else {
+        error_field[2].style.display = "none";
+        email.value = inputValue;
+    }
+
+
+
 });
