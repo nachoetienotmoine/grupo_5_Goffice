247bd2f93a3bc8ad57ae76f472c8c1eebb26f737
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-09-22 19:33:40 +0200

    actualizo slider
    actualizo slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -8,14 +8,14 @@ const swiper = new Swiper('.swiper', {
   
   
 
-  // If we need pagination
+  /*If we need pagination
     pagination: {
     el: '.swiper-pagination',
     clickable: true,
     dynamicBullets: true,
-  },
+  }, */
 
-  // Navigation arrows
+    //Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
