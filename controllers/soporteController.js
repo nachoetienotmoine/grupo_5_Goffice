7860942e8baece2dc5decf0ef731d6966bfbe3fd
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-09-22 23:07:53 -0300

    Avance vistas soporte

--- controllers/soporteController.js
+++ controllers/soporteController.js
@@ -4,6 +4,11 @@ const soporteController = {
     ayudar: (req, res) => {
         res.render("ayuda")
     },
+    ///Costo-Envio///
+    enviar: (req, res) => {
+        res.render("costoenvio")
+
+    },
      ///Contacto///
      contactar: (req, res) => {
         res.render("contacto")
--- routers/soporte.js
+++ routers/soporte.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const soporteController = require('../controllers/soporteController');
 
 router.get('/ayuda', soporteController.ayudar);
+router.get('/costoenvio', soporteController.enviar);
 router.get('/contacto', soporteController.contactar);
 
 module.exports = router;
