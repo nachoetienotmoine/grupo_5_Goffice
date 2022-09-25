19698e9ff1eeff86d404185105ca0223d0e10750
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-09-24 22:45:14 -0300

    Vistas de Soporte

--- controllers/soporteController.js
+++ controllers/soporteController.js
@@ -9,12 +9,34 @@ const soporteController = {
         res.render("costoenvio")
 
     },
+    ///Puntos-Retiro///
+    retirar: (req, res) => {
+        res.render("puntosretiro")
+
+    },    
      ///Contacto///
      contactar: (req, res) => {
         res.render("contacto")
+    },
+     ///Devoluciones///
+    devolver: (req, res) => {
+        res.render("devoluciones")
+
+    },
+    ///Terminos y condiciones///
+    determinar: (req, res) => {
+       res.render("terminos")
+
+    },
+    ///Privacidad///
+    privar: (req, res) => {
+      res.render("privacidad")
+
+    },
+    ///Arrepentimiento///
+    arrepentir: (req, res) => {
+     res.render("arrepentimiento")
     }
-
-
 }
 
 
--- routers/soporte.js
+++ routers/soporte.js
@@ -7,6 +7,11 @@ const soporteController = require('../controllers/soporteController');
 
 router.get('/ayuda', soporteController.ayudar);
 router.get('/costoenvio', soporteController.enviar);
+router.get('/puntoretiro', soporteController.retirar);
+router.get('/devoluciones', soporteController.devolver);
+router.get('/terminos', soporteController.determinar);
+router.get('/privacidad', soporteController.privar);
+router.get('/arrepentimiento', soporteController.arrepentir);
 router.get('/contacto', soporteController.contactar);
 
 module.exports = router;
