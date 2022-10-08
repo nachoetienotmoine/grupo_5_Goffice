da0c3608264a71b75dd7ad99e3e175672b8038f5
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-10-08 14:50:50 -0300

    js para tarejta

--- public/js/JSGeneral.js
+++ public/js/JSGeneral.js
@@ -1,17 +1,17 @@
-function scriptSrc(src){
+function scriptSrc(src) {
     let tag = document.createElement('script');
     tag.src = src;
     tag.defer = true;
     let firstScriptTag = document.getElementsByTagName('head')[0].appendChild(tag);
 }
 
-if (location.href === 'http://localhost:3000/admin/productos'){
+if (location.href === 'http://localhost:3000/admin/productos') {
     let aButtons = document.querySelectorAll(".botonEdit");
     aButtons.forEach(aButton => {
         aButton.addEventListener('click', (e) => {
             let href = aButton.href;
             let hrefLength = href.length - 1;
-            let id = href.slice(hrefLength,80);
+            let id = href.slice(hrefLength, 80);
             sessionStorage.setItem("id", id);
         });
     });
@@ -34,7 +34,7 @@ switch (location.href) {
     case 'http://localhost:3000/carrito':
         scriptSrc("/js/Carrito.js");
         scriptSrc("/js/HeaderSearchBar.js");
-        break; 
+        break;
     case 'http://localhost:3000/carrito/checkout':
         scriptSrc("/js/CheckOut.js");
         scriptSrc("/js/CheckoutValidations.js");
@@ -45,11 +45,15 @@ switch (location.href) {
         scriptSrc("/js/HeaderSearchBar.js");
         break;
     case `http://localhost:3000/admin/productos/${id}/edit`:
-            scriptSrc("/js/EditProductosValidations.js");
-            scriptSrc("/js/HeaderSearchBar.js");
-            break;
+        scriptSrc("/js/EditProductosValidations.js");
+        scriptSrc("/js/HeaderSearchBar.js");
+        break;
     case "http://localhost:3000/admin/productos/create":
-            scriptSrc("/js/ProductosValidations.js");
-            scriptSrc("/js/HeaderSearchBar.js");
-            break;
+        scriptSrc("/js/ProductosValidations.js");
+        scriptSrc("/js/HeaderSearchBar.js");
+        break;
+    case "http://localhost:3000/pagotarjeta":
+        scriptSrc("/js/Tarjeta.js");
+        scriptSrc("/js/HeaderSearchBar.js");
+        break;
 }
