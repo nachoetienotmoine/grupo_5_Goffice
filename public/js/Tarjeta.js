03be16746db2c9575c6f6df6f71656d96bf3a1bf
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-10-09 22:10:19 +0200

    terminando css y html
    finalizado etapa de html css credit card

--- public/js/Tarjeta.js
+++ public/js/Tarjeta.js
@@ -1,5 +1,50 @@
-const tarjeta = document.querySelector("#cardForm");
+const tarjeta = document.querySelector("#cardForm"),
+    btnOpenForm = document.querySelector("#btn-open-close"),
+    Form = document.querySelector("#creditForm");
 
+    //* rotacion tarjeta
 tarjeta.addEventListener("click" , () => {
     tarjeta.classList.toggle("active");
 });
+//* btn abrir form 
+btnOpenForm.addEventListener("click", () => {
+    btnOpenForm.classList.toggle("active");
+    Form.classList.toggle("active");
+});
+
+
+//* select month dinamic 
+
+for(let i = 1; i <= 12; i ++){
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerText = i;
+    Form.selectMonth.appendChild(option);
+}
+
+
+
+
+//* select year dinamic 
+
+const yearActual = new Date() .getFullYear();
+
+for(let i = yearActual; i <= yearActual + 8; i ++){
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerText = i;
+    Form.selectYear.appendChild(option);
+    
+}
+
+//* input nuumber card
+
+Form.inputNumber.addEventListener("KeyUp", (e) => {
+    console.log(e)
+
+
+})
+
+
+
+ 
