fee18cef88ebcac9ab11eeddfd9a4e0e46a54a1d
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-10-11 15:21:22 -0300

    cambios contraseña base de datos

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "R@cingclub5",
+    "password": "null",
     "database": "goffice_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
